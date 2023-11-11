f175209f17a42289dd03b776affa2fa08e4bb054
devnaano <naano.dev@gmail.com>
2023-11-11 13:34:50 -0300

    structuring website responsiveness for mobile devices

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ function changeStyleFocus(event) {
     let element = event.target.getAttribute('name');
     let formLabel = document.querySelector(`form label[for=${element}]`);
     formLabel.style.marginBottom = '0px';
-    formLabel.style.fontSize = '10px';
+    formLabel.style.fontSize = '9px';
     formLabel.style.color = '#117CB4';
     event.target.style.borderBottom = '2px solid #117CB4';
 }
@@ -22,7 +22,7 @@ function changeStyleBlur(event) {
     let formLabel = document.querySelector(`form label[for=${element}]`);
     if (event.target.value === "") {
         formLabel.style.marginBottom = '-7px';
-        formLabel.style.fontSize = '14px';
+        formLabel.style.fontSize = '12px';
         formLabel.style.color = '#aaa';
         event.target.style.borderBottom = '2px solid #aaa';
     } else {
